5001991c82d04f1d677cbe4503f29129fdbc2ce3
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-05 23:54:47 +0700

    update

--- main.go
+++
@@ -1,7 +0,0 @@
-package learn_go_backend
-
-import "fmt"
-
-func main() {
-	fmt.Println("Test successfully!")
-}
---
+++ main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 10
+	var f float32 = 20.5
+	var b bool = true
+	var s string = "Hà Nội"
+
+	fmt.Println(i)
+	fmt.Println(f)
+	fmt.Println(b)
+	fmt.Println(s)
+
+}
