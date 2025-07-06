10928a49cbdeb180d09ca07b34aa1c15e21394c9
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-06 09:56:06 +0700

    update

--- main/main.go
+++ main/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func sum(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func prod(a, b int) int {
+	return a * b
+}
+
 func main() {
 	var a int
 	fmt.Println("nhap vao so a: ")
@@ -10,7 +22,7 @@ func main() {
 	fmt.Println("nhap vao so b: ")
 	fmt.Scan(&b)
 
-	fmt.Printf("Tong cua 2 so a va b: %d\n", a+b)
-	fmt.Printf("Hieu cua 2 so a va b: %d\n", a-b)
-	fmt.Printf("Tich cua 2 so a va b: %d\n", a*b)
+	fmt.Printf("Tong cua 2 so a va b: %d\n", sum(a, b))
+	fmt.Printf("Hieu cua 2 so a va b: %d\n", sub(a, b))
+	fmt.Printf("Tich cua 2 so a va b: %d\n", prod(a, b))
 }
