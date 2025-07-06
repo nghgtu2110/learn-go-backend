db1b004290c7aacf733119c9c40f95455d52ba4a
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-06 10:03:51 +0700

    update

--- main/main.go
+++ main/main.go
@@ -2,27 +2,7 @@ package main
 
 import "fmt"
 
-func sum(a, b int) int {
-	return a + b
-}
-
-func sub(a, b int) int {
-	return a - b
-}
-
-func prod(a, b int) int {
-	return a * b
-}
-
 func main() {
-	var a int
-	fmt.Println("nhap vao so a: ")
-	fmt.Scan(&a)
-	var b int
-	fmt.Println("nhap vao so b: ")
-	fmt.Scan(&b)
-
-	fmt.Printf("Tong cua 2 so a va b: %d\n", sum(a, b))
-	fmt.Printf("Hieu cua 2 so a va b: %d\n", sub(a, b))
-	fmt.Printf("Tich cua 2 so a va b: %d\n", prod(a, b))
+	var i1 = 5
+	fmt.Printf("An integer: %d, it's location in memory: %p\n", i1, &i1)
 }
