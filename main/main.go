39a55dabfda0b25d557472b5e40e7d49a0d3463e
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-06 10:30:36 +0700

    update

--- main/main.go
+++ main/main.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	n := 42
-	// Use of control structure if and else to check whether number is even or not
-	if n%2 == 0 {
-		fmt.Printf("The value is even\n")
-	} else {
-		fmt.Printf("The value is odd\n")
+	max := 4
+	if val := 10; val > max {
+		fmt.Println("Bigger than max")
 	}
 }
