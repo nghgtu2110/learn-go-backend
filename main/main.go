e39779b9e64354b8686eb1e9804ae88ea49a6ea2
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-06 10:11:14 +0700

    update

--- main/main.go
+++ main/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	var i1 = 5
-	var i1P *int = &i1
-	fmt.Printf("An integer: %d, it's location in memory: %p\n", i1, i1P)
+	fmt.Printf("An integer: %d, its location in memory: %p\n", i1, &i1)
+	var intP *int // Pointer variable
+	intP = &i1    // Storing address of i1 in pointer variable
+	fmt.Printf("The value at memory location %p is %d\n", intP, *intP)
+
 }
