6d4e5075a73aae4de3b052a058d433d966483a63
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-06 10:18:11 +0700

    update

--- main/main.go
+++ main/main.go
@@ -1,14 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	// error of pointers and addresses:
-
-	const i = 5
-	ptr1 := &i  //error: cannot take the address of i
-	ptr2 := &10 //error: cannot take the address of 10
-
-	fmt.Printf("Here is the pointer p: %p\n", ptr1) // cannot print
-	fmt.Printf("Here is the pointer p: %p\n", ptr2) // cannot print
+	var p *int = nil // Making a nil pointer
+	*p = 0           // việc hủy tham chiếu con trỏ nill là không được phép.
 }
+
+// panic: runtime error: invalid memory address or nil pointer dereference
