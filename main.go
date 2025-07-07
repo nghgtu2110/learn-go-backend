c89b5090c3398bbe7d380688c52bdaf519a0dd0a
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-07 16:33:57 +0700

    update

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ func compute(fn func(x, y float64) float64, x, y float64) float64 {
 	return fn(x, y)
 }
 
+func mathpow(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
 
 	fmt.Println(compute(hypot, 5, 12))
+	fmt.Println(mathpow(math.Pow))
 }
