e5e3e941749be5b90f3b99f57572060e0fb430e0
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-09 10:02:24 +0700

    update

--- main.go
+++ main.go
@@ -1,40 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type I interface {
-	M()
-}
-
-type T struct {
-	S string
-}
-
-func (t *T) M() {
-	fmt.Println(t.S)
-}
-
-type F float64
-
-func (f F) M() {
-	fmt.Println(f)
-}
+import "fmt"
 
 func main() {
-	var i I
+	var i interface{} = "hello"
 
-	i = &T{"Hello"}
-	describe(i)
-	i.M()
+	s := i.(string)
+	fmt.Println(s)
 
-	i = F(math.Pi)
-	describe(i)
-	i.M()
-}
+	s, ok := i.(string)
+	fmt.Println(s, ok)
 
-func describe(i I) {
-	fmt.Printf("(%v, %T)\n", i, i)
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
 }
