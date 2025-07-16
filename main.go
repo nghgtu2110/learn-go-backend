bf536e4a513a997871ffcbecb3267d5996a3ea63
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-16 18:18:15 +0700

    goroutine

--- main.go
+++ main.go
@@ -5,24 +5,18 @@ import (
 	"time"
 )
 
-type Person struct {
-	Name       string
-	Occupation string
-	BirthYear  int
-}
-
-func (p Person) GetAge() int {
-	return time.Now().Year() - p.BirthYear
-}
-
-func (p Person) IsAppropriateCareer() bool {
-	return len(p.Name) >= 0 && (p.BirthYear%len(p.Name) == 0)
+func task(id int) {
+	fmt.Printf("task start %d\n", id)
+	time.Sleep(1 * time.Second)
+	fmt.Println("task end", id)
 }
 
 func main() {
-	var person Person
-	fmt.Scan(&person.Name, &person.Occupation, &person.BirthYear)
+	start := time.Now()
+	task(1)
+	task(2)
+	task(3)
+	task(4)
 
-	fmt.Println(person.GetAge())
-	fmt.Println(person.IsAppropriateCareer())
+	fmt.Println("Time to finish all tasks: ", time.Since(start))
 }
