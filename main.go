17a60c2ffa884b221b1386abbba9a4e39a375659
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-16 20:12:12 +0700

    goroutine

--- main.go
+++ main.go
@@ -2,26 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-func task(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("task start %d\n", id)
-	time.Sleep(1 * time.Second)
-	fmt.Println("task end", id)
-}
-
 func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
+	fmt.Println("Starting")
+	ch := make(chan int)
 
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go task(i, &wg)
-	}
+	go func() {
+		ch <- 10
+		ch <- 20
+		ch <- 30
+		fmt.Println("Sending...")
+	}()
 
-	wg.Wait()
-	fmt.Println("Time to finish all tasks: ", time.Since(start))
+	// Goroutine Anonymous
+	go func() {
+		fmt.Println(<-ch)
+		fmt.Println(<-ch)
+		fmt.Println(<-ch)
+		fmt.Println("Receiving...")
+	}()
+
+	// do anything on the channel isn't quick
+	// so fmt.Println() may complete sooner
+
+	fmt.Println("Ending")
+	time.Sleep(1 * time.Second)
 }
