c89c557dcdeea6a9f5142b4bae5583c26c2659e8
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-16 23:35:43 +0700

    update

--- main.go
+++ main.go
@@ -10,12 +10,16 @@ func main() {
 	ch := make(chan int)
 
 	go func() {
-		defer close(ch)
+		//defer close(ch)
 		ch <- 10
 		ch <- 20
 		ch <- 30
 		fmt.Println("Sending...")
 	}()
+
+	// error here
+	close(ch)
+	// main always starts earlier than other go routines
 
 	// only work when there is close the channel statement
 	for v := range ch {
