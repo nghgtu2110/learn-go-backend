a9be3e3fa945ceced00bd3da5bb5926994a5256c
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-16 23:33:34 +0700

    update

--- main.go
+++ main.go
@@ -10,26 +10,17 @@ func main() {
 	ch := make(chan int)
 
 	go func() {
+		defer close(ch)
 		ch <- 10
 		ch <- 20
 		ch <- 30
 		fmt.Println("Sending...")
 	}()
 
-	// Goroutine Anonymous
-	// go func() {
-	//	fmt.Println(<-ch)
-	//	fmt.Println(<-ch)
-	//	fmt.Println(<-ch)
-	//	fmt.Println("Receiving...")
-	//}()
-
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-	//fmt.Println(<-ch)
-
-	// do anything on the channel isn't quick
-	// so fmt.Println("") may complete sooner
+	// only work when there is close the channel statement
+	for v := range ch {
+		fmt.Println(v)
+	}
 
 	fmt.Println("Ending")
 	time.Sleep(1 * time.Second)
