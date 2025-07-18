94649f8c117e490ea14bd5f43748c79a35cfcf6d
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-18 10:59:40 +0700

    go back to leetcode two sum

--- main.go
+++ main.go
@@ -1,37 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"os"
-	"strings"
+	"leetcode/two_sum"
 )
 
 func main() {
-
-	inputFile, inputError := os.Open("input.txt")
-	if inputError != nil {
-		fmt.Printf("An error occurred on opening the inputfile\n" +
-			"Does the file exist?\n" +
-			"Have you got access to it?\n")
-		return // exit the function on error
-	}
-
-	defer inputFile.Close()
-
-	inputReader := bufio.NewReader(inputFile)
-
-	for {
-		inputString, readerError := inputReader.ReadString('|')
-		if readerError == io.EOF {
-			fmt.Printf("The input was: %s\n", inputString)
-			break // EOF reached after printing the last line
-		}
-		// Remove the trailing newline '\n' or any delimiter
-		inputString = strings.TrimSuffix(inputString, "|")
-		// Use TrimRight if you want to remove \r\n (Windows)
-		// line = strings.TrimRight(line, "\r\n")
-		fmt.Printf("The input was: %s\n", inputString)
-	}
+	fmt.Println(two_sum.TwoSum([]int{0, 2, 6, 5}, 7))
 }
