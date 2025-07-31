d233d5fed78a53b31d5574eae6505f281a067384
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-31 12:19:47 +0700

    race codition and waitgroup

--- main.go
+++ main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func main() {
-	ch1 := make(chan string)
-	ch2 := make(chan string)
-
-	go func() {
-		time.Sleep(1 * time.Second)
-		ch1 <- "Hello from ch1"
-	}()
-
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch2 <- "Hello from ch2"
-	}()
-
-	/*Nếu muốn nhận cả ch1 và ch2, bạn cần dùng for-select
-	Khi đó, chương trình sẽ chờ lần lượt 2 lần để nhận cả hai channel. */
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-ch1:
-			fmt.Println("Received from ch1:", msg1)
-		case msg2 := <-ch2:
-			fmt.Println("Received from ch2:", msg2)
-		}
+// use sync.Mutex Lock for clean code
+func sum(sp *int, n int) {
+	for i := 1; i <= n; i++ {
+		*sp += 1
 	}
+}
 
+func main() {
+
+	s := 0
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		fmt.Println("First sum ")
+		sum(&s, 10000) // 100 000 will result in race condition
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("Second sum ")
+		sum(&s, 10000)
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println("Total sum is: ", s)
 }
