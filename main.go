f300c6fc34eac487ca1f3f36d865b585709ec935
nghgtu2110 <ng.hoangtu.main@gmail.com>
2025-07-31 17:23:24 +0700

    test multiple WaitGroup.Wait() with time.Since

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // use sync.Mutex Lock for clean code
@@ -13,24 +14,34 @@ func sum(sp *int, n int) {
 }
 
 func main() {
-
+	time_start := time.Now()
 	s := 0
 
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
+		time_start := time.Now()
 		fmt.Println("First sum ")
-		sum(&s, 10000) // 100 000 will result in race condition
+		sum(&s, 100000) // 100 000 will result in race condition
+		elapsed := time.Since(time_start)
+		fmt.Println("Time taken: ", elapsed)
 		wg.Done()
 	}()
+	wg.Wait()
 
 	wg.Add(1)
 	go func() {
+		time_start := time.Now()
 		fmt.Println("Second sum ")
-		sum(&s, 10000)
+		sum(&s, 100000)
+		elapsed := time.Since(time_start)
+		fmt.Println("Time taken: ", elapsed)
 		wg.Done()
 	}()
 
 	wg.Wait()
 	fmt.Println("Total sum is: ", s)
+
+	elapsed := time.Since(time_start)
+	fmt.Println("Time taken: ", elapsed)
 }
